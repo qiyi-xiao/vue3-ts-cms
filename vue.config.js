4b3f2fb9c865dd7b1e06b6bb457c4fdd7a441182
qiying_xiao <qiying_xiao@126.com>
2025-01-19 23:14:04 +0800

    vue_condig_js配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,37 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+// const path = require('path')
+module.exports = {
+  outputDir: './build', // 构建时输出的目录
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: '@/components'
+      }
+    }
+  }
+
+  // chainWebpack: (config) => {
+  //   config.resolve.alias
+  //     .set('@', path.resolve(__dirname, 'src'))
+  //     .set('components', '@/components')
+  // }
+
+  // 配置方式二
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     components: '@/components'
+  //   }
+  // }
+
+  // 配置方式三
+  // chainWebpack: (config) => {
+  //   config.resolve.alias
+  //     .set('@', path.resolve(__dirname, 'src'))
+  //     .set('components', '@/components')
+  // }
+}
